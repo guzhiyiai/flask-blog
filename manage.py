9ee09327bcb37880b67f1c205035c9b8cfa21819
guzhiyiai <guzhiyiai@gmail.com>
2014-11-25 16:13:09 +0800

    Myblog: refactoring manage.py

--- manage.py
+++ manage.py
@@ -4,34 +4,30 @@ from flask_script import Manager
 from flask_script import Server
 from flask_script import prompt_bool
 
-from app.myapp import create_app
+from app.application import create_app
 from app.extensions import db
 from app import models
 
 app = create_app()
 
 # flask_script -> add application of command line parameters
-# Calling manager.run() prepares your Manager instance to receive input from the command line.
-# called -> python manage.py runserver
-
 manager = Manager(app)
+
 
 @manager.command
 def createall():
     "Creates database tables"
     db.create_all()
 
+
 @manager.command
 def dropall():
     "Drops all database tables"
-
-    if prompt_bool("Are you sure ? You will lose all your data !"):
+    if prompt_bool("Are you sure ? You will lose all your data!"):
         db.drop_all()
 
 manager.add_command("runserver", Server(host='0.0.0.0', port=5555))
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     manager.run()
-
-
