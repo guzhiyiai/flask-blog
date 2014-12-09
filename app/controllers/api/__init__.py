e6a73fc62bd391e51d7f54f9512c1bd55592a27d
guzhiyiai <guzhiyiai@gmail.com>
2014-12-09 17:58:05 +0800

    api: design response

--- app/controllers/api/__init__.py
+++ app/controllers/api/__init__.py
@@ -5,5 +5,14 @@ from flask import Blueprint
 bp = Blueprint('api', __name__)
 
 
+class APIError:
+
+    OK = (200, 'OK')
+    CREATE = (201, 'created_successfully')
+    BAD_REQUEST = (400, 'bad_request')
+    BAD_USER = (401, 'unauthorized')
+    NO_ACCESS = (403, 'forbidden')
+    NOT_FOUND = (404, 'not_found')
+
 from . import comment
 from . import post
