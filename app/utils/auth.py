2132d75de9a74749eb67d41565c1c0ac3b9df4d6
guzhiyiai <guzhiyiai@gmail.com>
2015-04-01 17:24:49 +0800

    auth: user cookies-based auth

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,10 +1,25 @@
 from functools import wraps
 from flask import session, redirect, url_for
 
+
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if 'email' not in session:
-	    return redirect(url_for('.signin'))
-	return f(*args, **kwargs)
+        if 'admin_uid' not in session:
+            return redirect(url_for('.login'))
+        return f(*args, **kwargs)
     return decorated_function
+
+
+def login_admin(user_id):
+    session.permanent = True
+    session['admin_uid'] = user_id
+
+
+def logout_admin():
+    session.pop('admin_uid', None)
+
+
+def current_user():
+    pass
+
