e189da5de0ae70fddb85cab117a8744c94db1ddb
guzhiyiai <guzhiyiai@gmail.com>
2015-04-01 17:21:55 +0800

    models: modify the user and password default size

--- app/models.py
+++ app/models.py
@@ -18,14 +18,13 @@ class User(db.Model):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50))
-    password = db.Column(db.String(300))
+    username = db.Column(db.String(64))
+    password = db.Column(db.String(128))
     email = db.Column(db.String(100), unique=True)
     role = db.Column(db.Integer, index=True,
                      default=UserRole.USER, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
-    # posts = db.relationship('Post')
 
     def to_dict(self):
         return dict(
@@ -51,7 +50,6 @@ class Post(db.Model):
     status = db.Column(db.Integer, default=PostStatus.PENDING, index=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
-    # comments = db.relationship('Comment')
 
     user_id = db.Column(db.Integer)
 
