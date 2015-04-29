bde87f467bf481e64648ae6df94f4a503fe25141
guzhiyiai <guzhiyiai@gmail.com>
2015-04-29 11:20:38 +0800

    manage: modify the default config

--- manage.py
+++ manage.py
@@ -7,33 +7,14 @@ from flask_script import Manager
 from flask_script import Server
 from flask_script import prompt_bool
 
-from app.settings import DevelopmentConfig
-from app.settings import StagingConfig
-from app.settings import ProductionConfig
+from app.settings import config
 
 from app.application import create_app
 from app.extensions import db
 from app import models
 
 
-# create app with env
-config_map = {
-    'dev': DevelopmentConfig,
-    'stag': StagingConfig,
-    'prod': ProductionConfig
-}
-
-# set env via hostnave/environment variable
-hostname = socket.gethostname()
-flask_env = 'prod'
-if 'stag' in hostname:
-    flask_env = 'stag'
-elif hostname.endswith('local'):
-    flask_env = 'dev'
-flask_env = os.environ.get('FLASK_ENV', flask_env)
-
-
-app = create_app(config_map[flask_env])
+app = create_app(config['development'])
 
 # flask_script -> add application of command line parameters
 manager = Manager(app)
